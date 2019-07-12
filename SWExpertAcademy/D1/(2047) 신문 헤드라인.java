fa41bef14f4f9d407bd51f7d17b335bd4f63aeb1
gospel306 <46041710+gospel306@users.noreply.github.com>
2019-07-12 10:43:15 +0900

    Create (2047) 신문 헤드라인.java

---
+++ SWExpertAcademy/D1/(2047) 신문 헤드라인.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Solution {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String s = sc.nextLine();
+		s = s.toUpperCase();
+		System.out.print(s);
+	}
+}
