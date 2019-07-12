86e298ff11678a866b693f23189a5da9ed5b46ce
gospel306 <46041710+gospel306@users.noreply.github.com>
2019-07-12 10:43:53 +0900

    Create (2046) 스탬프 찍기.java

---
+++ SWExpertAcademy/D1/(2046) 스탬프 찍기.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Solution {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int num = sc.nextInt();
+		for(int i = 0;i < num;i++)
+			System.out.print("#");
+	}
+}
