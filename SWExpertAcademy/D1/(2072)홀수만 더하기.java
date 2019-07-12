40792e06dd551137e5a2a6e771ced0c8c9f9faa6
gospel306 <46041710+gospel306@users.noreply.github.com>
2019-07-12 10:36:28 +0900

    Create (2072)홀수만 더하기.java

---
+++ SWExpertAcademy/D1/(2072)홀수만 더하기.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Solution {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int testcase = sc.nextInt();
+		int num,sum = 0;
+		for(int i = 0;i < testcase;i++) {
+			for(int j = 0;j<10;j++) {
+				num = sc.nextInt();
+				if(num % 2 == 1)
+					sum += num;
+			}
+			System.out.printf("#%d %d\n",(i+1),sum);
+      sum = 0;
+		}
+	}
+}
