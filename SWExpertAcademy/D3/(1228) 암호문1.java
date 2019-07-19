75c27b8d342d8cd102494d2ec5218eca83b1d31c
gospel306 <46041710+gospel306@users.noreply.github.com>
2019-07-19 11:39:38 +0900

    Create (1228) 암호문1.java

---
+++ SWExpertAcademy/D3/(1228) 암호문1.java
@@ -0,0 +1,26 @@
+import java.util.LinkedList;
+import java.util.Scanner;
+
+public class Solution {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		for (int t = 1; t < 11; t++) {
+            LinkedList<Integer> list = new LinkedList<>();
+			int size = sc.nextInt();
+			for (int i = 0; i < size; i++)
+				list.add(sc.nextInt());
+			int a = sc.nextInt();
+			for (int i = 0; i < a; i++) {
+				String string = sc.next();
+				int where = sc.nextInt();
+				int howmany = sc.nextInt();
+				for (int j = 0; j < howmany; j++)
+					list.add(where++, sc.nextInt());
+			}
+			System.out.printf("#%d ",t);
+			for (int i = 0; i < 10; i++)
+				System.out.print(list.get(i) + " ");
+			System.out.println();
+		}
+	}
+}
