33057aaf23b0744c21d39dcf6a7116ce25afafcc
김동욱 <46041710+gospel306@users.noreply.github.com>
2020-01-02 14:22:50 +0900

    Update (5789) 현주의 상자 바꾸기.java

--- SWExpertAcademy/D3/(5789) 현주의 상자 바꾸기.java
+++ SWExpertAcademy/D3/(5789) 현주의 상자 바꾸기.java
@@ -1,1 +1,24 @@
+import java.util.Scanner;
 
+public class Solution {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int testcase = sc.nextInt();
+		for (int t = 1; t <= testcase; t++) {
+			int N = sc.nextInt();
+			int Q = sc.nextInt();
+			int[] box = new int[N];
+			for (int i = 0; i < Q; i++) {
+				int L = sc.nextInt();
+				int R = sc.nextInt();
+				for (int j = L - 1; j < R; j++) {
+					box[j] = i + 1;
+				}
+			}
+			System.out.print("#"+t+" ");
+			for(int i = 0;i < box.length;i++)
+				System.out.print(box[i]+" ");
+			System.out.println();
+		}
+	}
+}
