3b0ee07a73c420811c8971a3da69d652afa1c760
김동욱 <46041710+gospel306@users.noreply.github.com>
2020-01-02 14:24:14 +0900

    Update (5948) 새샘이의 7-3-5 게임.java

--- SWExpertAcademy/D3/(5948) 새샘이의 7-3-5 게임.java
+++ SWExpertAcademy/D3/(5948) 새샘이의 7-3-5 게임.java
@@ -1,1 +1,29 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Scanner;
 
+public class Solution {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int testcase = sc.nextInt();
+		List<Integer> list = new ArrayList<>();
+		HashSet<Integer> set = new HashSet<>();
+		for(int t = 1;t <= testcase;t++) {
+			int[] num = new int[7];
+			for(int i = 0;i < 7;i++)
+				num[i] = sc.nextInt();
+			list.clear();
+			set.clear();
+			for(int i = 0;i < 7;i++)
+				for(int j = i+1;j<7;j++)
+					for(int k = j+1;k<7;k++) {
+						set.add(num[i]+num[j]+num[k]);
+					}
+			list.addAll(set);
+			Collections.sort(list);
+			System.out.println("#"+t+" "+list.get(list.size()-5));
+		}
+	}
+}
