b3429eeed9950193ea36436be9985ef9b9a131cc
김동욱 <46041710+gospel306@users.noreply.github.com>
2020-05-14 12:32:42 +0900

    Create (11720) 숫자의 합.java

---
+++ BaekJoon/(11720) 숫자의 합.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int size = sc.nextInt();
+		String num = sc.next();
+		int result = 0;
+		for(int i = 0;i < size;i++) {
+			result += num.charAt(i) - '0';
+		}
+		System.out.println(result);
+	}
+}
