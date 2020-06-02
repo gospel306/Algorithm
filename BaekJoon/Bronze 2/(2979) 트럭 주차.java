99b22add3b2daa4a0203975c5837d7d13ee254e1
김동욱 <46041710+gospel306@users.noreply.github.com>
2020-06-02 10:46:16 +0900

    Create (2979) 트럭 주차.java

---
+++ BaekJoon/Bronze 2/(2979) 트럭 주차.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int A = sc.nextInt();
+		int B = sc.nextInt();
+		int C = sc.nextInt();
+		int[] arr = new int[101];
+		int ans = 0;
+		int n = 0;
+		for(int i = 0;i < 3;i++) {
+			int start = sc.nextInt();
+			int end = sc.nextInt();
+			for(int j = start;j < end;j++)
+				arr[j]++;
+			n = Math.max(end, n);
+		}
+		for(int i = 1;i <= n;i++) {
+			if(arr[i] == 1)
+				ans += A;
+			else if(arr[i] == 2)
+				ans += B*2;
+			else if(arr[i] == 3)
+				ans += C*3;
+		}
+		System.out.println(ans);
+	}
+}
