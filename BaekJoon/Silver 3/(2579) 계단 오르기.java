b4a5a825bf6fb1aa073704d58331d07e650ebcfa
김동욱 <46041710+gospel306@users.noreply.github.com>
2020-06-02 10:27:01 +0900

    Update (2579) 계단 오르기.java

--- BaekJoon/Silver 3/(2579) 계단 오르기.java
+++ BaekJoon/Silver 3/(2579) 계단 오르기.java
@@ -2,7 +2,6 @@ import java.util.Scanner;
 
 public class Main {
 	static int N;
-	static int[][] arr, dir = {{0,1},{-1,0},{0,-1},{1,0}};
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		N = sc.nextInt();
