22406783e06366d90280ffbaf3004b89907fec20
김동욱 <46041710+gospel306@users.noreply.github.com>
2020-06-05 11:06:07 +0900

    Update 가운데 글자 가져오기.java

--- programmers/Level1/가운데 글자 가져오기.java
+++ programmers/Level1/가운데 글자 가져오기.java
@@ -1,6 +1,6 @@
 class Solution {
     public String solution(String s) {
         int n = s.length();
-		    return n%2 == 0 ? s.substring(n/2-1,n/2+1):s.substring(n/2,n/2+1);
+	return n%2 == 0 ? s.substring(n/2-1,n/2+1):s.substring(n/2,n/2+1);
     }
 }
